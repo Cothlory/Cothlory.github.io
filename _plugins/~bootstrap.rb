0e20799aa462f076e99a0c947629cb6e19cd8109
Chang Huang <cothlory@gmail.com>
2024-09-30 23:48:37 -0400

    modify bootstrap plugin

--- _plugins/~bootstrap.rb
+++ _plugins/~bootstrap.rb
@@ -17,11 +17,6 @@ module Jekyll
 			FileUtils.mkdir_p BOOTSTRAP_DIR
 			read_env
 			read_commit
-			read_katex_version
-			write_crossref_config
-			write_rouge_config
-			write_katex_config
-			fetch_mastodon_post
 			read_github_run_id
 		end
 
@@ -32,24 +27,6 @@ module Jekyll
 			end
 		end
 
-		def read_katex_version
-			@site.config['katex_version'] = `pandoc-katex --katex-version`.chomp
-		end
-
-		def write_crossref_config
-			yaml = YAML.dump @site.config['paru']['crossref'].transform_keys { _1.to_s.gsub(/_(\w)/) { $1.upcase } }
-			File.write File.join(BOOTSTRAP_DIR, 'crossref.yml'), yaml.tap { _1["---\n"] = '' }
-		end
-
-		def write_rouge_config
-			File.write File.join(BOOTSTRAP_DIR, 'rouge.yml'), YAML.dump(@site.config['paru']['rouge'])
-		end
-
-		def write_katex_config
-			ENV['PANDOC_KATEX_CONFIG_FILE'] = filename = File.join BOOTSTRAP_DIR, 'katex.toml'
-			File.write filename, Tomlib.dump(@site.config['paru']['katex'])
-		end
-
 		def read_env
 			@site.config['avoid_markdown'] = !!ENV['JEKYLL_AVOID_MARKDOWN']
 			@site.config['no_archive'] = !!ENV['JEKYLL_NO_ARCHIVE']
@@ -58,10 +35,6 @@ module Jekyll
 		def read_commit
 			@site.config['commit_hash'] = `git rev-parse HEAD`.chomp
 			@site.config['commit_time'] = Time.at `git show -s --format=%ct`.to_i
-		end
-
-		def fetch_mastodon_post
-			@fetch_mastodon_post = UlyssesZhan::FetchMastodonPost.new @site
 		end
 
 		def read_github_run_id
